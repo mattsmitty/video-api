5a1798426bd7e65f289c0fae53ab61739d8e8d86
Manthan Damania <mntdamania@gmail.com>
2012-07-19 22:19:28 +0530

    re enable auth

--- app/admin/video.rb
+++ app/admin/video.rb
@@ -1,9 +1,5 @@
 ActiveAdmin.register Video do
 
-  controller do
-    before_filter :authenticate_admin_user!, :except => [:index]
-  end
-
   form do |f|
     f.inputs "Details" do 
       f.input :category_id, :as => :select, :collection => Category.all
