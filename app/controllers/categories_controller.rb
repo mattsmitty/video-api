cda6ffd92b8dd1bbc6b1886e5550ad3ef136ffc5
Manthan Damania <mntdamania@gmail.com>
2012-07-27 22:21:46 +0530

    index actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,10 @@
 class CategoriesController < ApplicationController
 
+  def index
+    app = App.find(params[:app_id])
+    @categories = app.categories
+  end
+
 
   def show
     @category = Category.find(params[:id])
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,11 @@
 class VideosController < ApplicationController
 
+  def index
+    app = App.find(params[:app_id])
+    category = app.categories.find(params[:category_id])
+    @videos = category.videos
+  end
+
 
   def show
     @video = Video.find(params[:id])
